80a8c45249e8b7b07f1a7ca9d10f51e149791e74
KasenBob <826107692@qq.com>
2019-08-02 20:55:31 +0800

    update requirements

--- web_manage/manager/models.py
+++ web_manage/manager/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 
 # Create your models here.
-"""
-登录信息表（账号，密码，ip地址，权限）
-"""
-
+#院系信息
 class depart_info(models.Model):
 	depart_name = models.CharField(max_length=25, primary_key=True)
 
+#专业信息
 class major_info(models.Model):
 	major_name = models.CharField(max_length=25, primary_key=True)
 
+#年级信息
 class grade_info(models.Model):
 	grade_name = models.CharField(max_length=25, primary_key=True)
 
+#班级信息
 class class_info(models.Model):
 	class_name = models.CharField(max_length=10, primary_key=True)
 
+#学生基本信息
 class stu_basic_info(models.Model):
 	stu_number = models.CharField(max_length=25, primary_key=True)
 	stu_name = models.CharField(max_length=25)
@@ -32,6 +33,7 @@ class stu_basic_info(models.Model):
 	photo = models.ImageField(upload_to='photo', null=True, blank=True)
 	stu_card_photo = models.ImageField(upload_to='stu_card_photo', null=True, blank=True)
 
+#用户登录信息
 class user_login_info(models.Model):
 	account = models.CharField(max_length=25, primary_key=True)
 	psword = models.CharField(max_length=25)
@@ -40,9 +42,11 @@ class user_login_info(models.Model):
 	ip = models.CharField(max_length=25)
 	jurisdiction = models.CharField(max_length=5)
 
+#职称信息
 class profess_info(models.Model):
 	profess_name = models.CharField(max_length=10, primary_key=True)
 
+#指导教师基本信息
 class teach_basic_info(models.Model):
 	tea_number = models.CharField(max_length=25, primary_key=True)
 	tea_name = models.CharField(max_length=25)
@@ -53,3 +57,33 @@ class teach_basic_info(models.Model):
 	email = models.EmailField(max_length=255, unique=True, null=True, blank=True)
 	phone_number = models.CharField(max_length=25, null=True, blank=True)
 	photo = models.ImageField(upload_to='photo', null=True, blank=True)
+
+#竞赛基本信息
+class com_basic_info(models.Model):
+	com_id = models.AutoField(primary_key=True)
+	com_name = models.CharField(max_length=50)
+	begin_regit = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+	end_regit = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+	begin_time = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+	end_time = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+	num_stu = models.IntegerField(null=True, blank=True)
+	need_full = models.IntegerField(null=True, blank=True)
+	same_stu = models.IntegerField(null=True, blank=True)
+	com_web = models.CharField(max_length=225, null=True, blank=True)
+	if_web = models.IntegerField(null=True, blank=True)
+	num_teach = models.IntegerField(null=True, blank=True)
+
+#竞赛小组信息
+class com_group_basic_info(models.Model):
+	com_id = models.ForeignKey('com_basic_info', to_field='com_id', on_delete=models.DO_NOTHING)
+	group_id = models.IntegerField()
+	group_name = models.CharField(max_length=25,null=True, blank=True)
+	group_num = models.IntegerField(null=True, blank=True)
+	class Meta:
+		unique_together = ('com_id', 'group_id')
+
+#竞赛学生信息
+class com_stu_info(models.Model):
+	com_id = models.ForeignKey('com_basic_info', to_field='com_id', on_delete=models.DO_NOTHING)
+	group_id = models.IntegerField(null=True, blank=True)
+	stu_id = models.ForeignKey('stu_basic_info', to_field='stu_number', on_delete=models.DO_NOTHING)
